025b6e8fc482f5fd149948674c86b5a3f73548e6
André Stephano <andrestephano@gmail.com>
2020-12-28 21:55:36 -0300

    finish 3

--- 3.py
+++ 3.py
@@ -142,5 +142,93 @@ print(loss)
 
 
 # %%
-labels
+outputs.shape[0]
+# %%
+
+
+def evaluate(model, val_loader):
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    optimizer = opt_func(model.parameters(), lr)
+    history = []  # for recording epoch-wise results
+
+    for epoch in range(epochs):
+
+        # Training Phase
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        # Validation phase
+        result = evaluate(model, val_loader)
+        model.epoch_end(epoch, result)
+        history.append(result)
+
+    return history
+
+# %%
+class MnistModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, xb):
+        xb = xb.reshape(-1, 784)
+        out = self.linear(xb)
+        return out
+
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['val_loss'], result['val_acc']))
+
+
+model = MnistModel()
+
+# %%
+result0 = evaluate(model, val_loader)
+result0
+# %%
+history1 = fit(5, 0.001, model, train_loader, val_loader)
+# %%
+history2 = fit(5, 0.001, model, train_loader, val_loader)
+# %%
+history3 = fit(10, 0.001, model, train_loader, val_loader)
+
+# %%
+history4 = fit(5, 0.001, model, train_loader, val_loader)
+
+# %%
+history5 = fit(5, 0.001, model, train_loader, val_loader)
+
+# %%
+history = [result0] + history1 + history2 + history3 + history4
+accuracies = [result['val_acc'] for result in history]
+plt.plot(accuracies, '-x')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.title('Accuracy vs. No. of epochs')
 # %%
